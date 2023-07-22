3b46faa106972466c961b811206bdf80535d9cc6
Tohidujjaman Hoque <tohi7479@gmail.com>
2023-07-22 20:18:59 +0530

    Starting Binary tree III

---
+++ Binary Tree III/3.constructinorderandPostorder.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+    public:
+    T val;
+    TreeNode<T> *left;
+    TreeNode<T> *right;
+
+    TreeNode(T data) {
+        this->val = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+ int posts;
+TreeNode<int>* f(vector<int>& postOrder, vector<int>& inOrder,int is,int ie){
+     if(is>ie || posts<0) return NULL;
+     TreeNode<int>* root=new TreeNode<int>(postOrder[posts--]);
+     int inIn;
+     for(int i=is;i<=ie;i++){
+          if(root->data==inOrder[i]){
+               inIn=i;
+               break;
+          }
+     }
+     root->right=f(postOrder,inOrder,inIn+1,ie);
+     root->left=f(postOrder,inOrder,is,inIn-1);
+     return root;
+}
+TreeNode<int>* getTreeFromPostorderAndInorder(vector<int>& postOrder, vector<int>& inOrder) 
+{
+	// Write your code here.
+     posts=postOrder.size()-1;
+     int is=0;
+     int ie=inOrder.size()-1;
+     return f(postOrder,inOrder,is,ie);
+}
