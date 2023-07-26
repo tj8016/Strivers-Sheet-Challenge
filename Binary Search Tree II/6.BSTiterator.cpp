811efdd18549f86f354a5ea6f350ac04b786aa54
Tohidujjaman Hoque <tohi7479@gmail.com>
2023-07-26 23:48:06 +0530

    Almost complete bst II

---
+++ Binary Search Tree II/6.BSTiterator.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+template <typename T>
+class TreeNode {
+    public:
+    T data;
+    TreeNode<T> *left;
+    TreeNode<T> *right;
+
+    TreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class BSTiterator
+{
+    public: 
+    stack<int> st;
+    void inorder(TreeNode<int>* root)
+    {
+        if(root==NULL)
+        return;
+
+        inorder(root->right);
+        st.push(root->data);
+        inorder(root->left);
+        return;
+    }
+    BSTiterator(TreeNode<int> *root)
+    {
+        // write your code here
+        inorder(root);
+    }
+
+    int next()
+    {
+        // write your code here
+        if(st.empty())
+        return -1;
+        else
+        {
+            int z=st.top();
+            st.pop();
+            return z;
+        } 
+
+    }
+
+    bool hasNext()
+    {
+        // write your code here
+        if(st.empty())
+        return false;
+        else
+        return true;
+    }
+};
