307c5534a3627ac0d2697a27e0a9c9d97c047b21
Tohidujjaman Hoque <tohi7479@gmail.com>
2023-07-30 23:21:15 +0530

    Completed DP

---
+++ DP/1.maximumProductSubarray.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+int maximumProduct(vector<int> &arr, int n)
+{
+	// Write your code here
+	int maxi=INT_MIN;
+	int mul=1;
+	for(int i=0;i<n;i++){
+		mul=mul*arr[i];
+		maxi=max(maxi,mul);
+		if(mul==0) mul=1;
+	}
+	 mul=1;
+	for(int i=n-1;i>=0;i--){
+		mul=mul*arr[i];
+		maxi=max(maxi,mul);
+		if(mul==0) mul=1;
+	}
+	return maxi;
+}
+
+int main() {
+    return 0;
+}
